e85cfcb85008d14b165469ff07d4bf8e8115f05f
Oskar Tyniec <39619380+Rakso12@users.noreply.github.com>
2021-03-15 00:29:31 +0100

    New features for edit dialog
    Adding small layout fix.
    Adding function to seting tmp variables for Tournament.

---
+++ tournamentinfo.h
@@ -0,0 +1,43 @@
+#ifndef TOURNAMENTINFO_H
+#define TOURNAMENTINFO_H
+#include "QString"
+
+class TournamentInfo
+{
+    QString tournamentAddress;
+    QString tournamentDate;
+    qint16 maxMembersLimit;
+    QString minCategory;
+    QString maxCategory;
+    float winPointsAssignment;
+    float losePointsAssignment;
+    float drawPointsAssignment;
+    float byePointsAssignment;
+    QString pairingSystem;
+
+public:
+    TournamentInfo();
+    QString getTournamentAddress();
+    QString getTournamentDate();
+    qint16 getMaxMembersLimit();
+    QString getMinCategory();
+    QString getMaxCategory();
+    float getWinPointsAssignment();
+    float getLosePointsAssignment();
+    float getDrawPointsAssignment();
+    float getByePointsAssignment();
+    QString getPairingSystem();
+
+    void setTournamentAddress(QString str);
+    void setTournamentDate(QString str);
+    void setMaxMembersLimit(qint16 number);
+    void setMinCategory(QString str);
+    void setMaxCategory(QString str);
+    void setWinPointsAssignment(float points);
+    void setLosePointsAssignment(float points);
+    void setDrawPointsAssignment(float points);
+    void setByePointsAssignment(float points);
+    void setPairingSystem(QString str);
+};
+
+#endif // TOURNAMENTINFO_H
